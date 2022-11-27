d043ae4e741fa2870479a6c4210ae2d7224a0a61
Ryan Whitmore <ryan.whit@outlook.com>
2022-11-27 15:25:40 -0800

    [#38] Fixed Event model imports. SingleEvent page now works.

--- client/src/pages/SingleEvent.js
+++ client/src/pages/SingleEvent.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-import { QUERY_SINGLE_EVENT } from '../utils/queries';
+import { QUERY_SINGLE_EVENT } from "../utils/queries";
 
 const SingleEvent = () => {
   const { eventId } = useParams();
@@ -13,55 +13,30 @@ const SingleEvent = () => {
   const { loading, data } = useQuery(QUERY_SINGLE_EVENT, {
     variables: { eventId: eventId },
   });
-  
+
   const event = data?.event || {};
 
   if (loading) {
     return <div>Loading...</div>;
   }
   console.log("query data: ", data);
-  // return (
-  //   <div className="my-3">
-  //     <h3 className="card-header bg-dark text-light p-2 m-0">
-  //       {event.creator} <br />
-  //       <span style={{ fontSize: '1rem' }}>
-  //         had this event on {event.date}
-  //       </span>
-  //     </h3>
-  //     <div className="bg-light py-4">
-  //       <blockquote
-  //         className="p-4"
-  //         style={{
-  //           fontSize: '1.5rem',
-  //           fontStyle: 'italic',
-  //           border: '2px dotted #1a1a1a',
-  //           lineHeight: '1.5',
-  //         }}
-  //       >
-  //         {event.description}
-  //       </blockquote>
-  //     </div>
-  //   </div>
-  // );
   return (
     <div className="">
       <h3 className="">
-         "Event Name<br />
-        <span style={{ fontSize: '1rem' }}>
-          had this event on Some Date
-        </span>
+        {event.creator} <br />
+        <span style={{ fontSize: "1rem" }}>had this event on {event.date}</span>
       </h3>
       <div className="">
         <blockquote
-          className=""
+          className="p-4"
           style={{
-            fontSize: '1.5rem',
-            fontStyle: 'italic',
-            border: '2px dotted #1a1a1a',
-            lineHeight: '1.5',
+            fontSize: "1.5rem",
+            fontStyle: "italic",
+            border: "2px dotted #1a1a1a",
+            lineHeight: "1.5",
           }}
         >
-          Event Description
+          {event.description}
         </blockquote>
       </div>
     </div>
