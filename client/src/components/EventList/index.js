710e9d158869364697524656211c8daaa0f0f167
Ryan Whitmore <ryan.whit@outlook.com>
2022-11-28 20:58:27 -0800

    [#67] Add link to single event page.

--- client/src/components/EventList/index.js
+++ client/src/components/EventList/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {
-  Grid,
-  Popup,
-  Button,
-  Header,
-} from "semantic-ui-react";
+import { Grid, Popup, Button, Header } from "semantic-ui-react";
 
 const EventList = ({ events }) => {
   console.log(events);
@@ -29,12 +24,15 @@ const EventList = ({ events }) => {
                 hoverable
               >
                 <Grid.Column textAlign="center">
-                  <Link to={`/users/${event.creator}`}> 
-                  <Header as="h4">Creator: {event.creator}</Header>
+                  <Link to={`/users/${event.creator}`}>
+                    <Header as="h4">Creator: {event.creator}</Header>
                   </Link>
                   <p>{event.description}</p>
                   <Button color="green">Reschedule</Button>
                   <Button color="red">Cancel</Button>
+                  <Link to={`/events/${event._id}`}>
+                    <Button color="blue">Details</Button>
+                  </Link>
                 </Grid.Column>
               </Popup>
             </Grid.Column>
