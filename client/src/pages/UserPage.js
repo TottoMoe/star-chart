76d970ad415eb475b493e189a86915f79bbdf9af
Ryan Whitmore <ryan.whit@outlook.com>
2022-11-29 18:12:24 -0800

    Updated user page fixes after merge conflict was resolved.

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -2,11 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import {
-  Grid,
-  Container,
-  Header
-} from "semantic-ui-react";
+import { Grid, Container, Header } from "semantic-ui-react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import EventList from "../components/EventList";
@@ -28,30 +24,30 @@ const UserPage = () => {
   }
   console.log("query data: ", data);
   return (
-    <main style={{Height: "100%", padding: "1em 0em"}}>
-      <Container style={{ background: "#0d0d0d",  margin: "10rem", color: "black",  }}>
+    <main style={{ Height: "100%", padding: "1em 0em" }}>
+      <Container
+        style={{ background: "#0d0d0d", margin: "10rem", color: "black" }}
+      >
         <Grid centered celled columns="equal" divided="vertically">
           <Grid.Column columns={2}>
             <Grid.Row width={5}>
-            <Header as="h2" color="blue" textAlign="center">
-            {user.username}
-            </Header>
-            <Header as="h3" color="blue" textAlign="center">
-            ID:{user._id}
-            </Header>
-              
+              <Header as="h2" color="blue" textAlign="center">
+                {user.username}
+              </Header>
+              <Header as="h3" color="blue" textAlign="center">
+                ID:{user._id}
+              </Header>
             </Grid.Row>
           </Grid.Column>
 
-            <Grid.Column floated="right" width={10}>
-              <Calendar onChange={onChange} value={value} />
-            </Grid.Column>
+          <Grid.Column floated="right" width={10}>
+            <Calendar onChange={onChange} value={value} />
+          </Grid.Column>
 
-            <Grid.Row>
-              <EventList events={user.createdEvents} title="Event title..." />
-            </Grid.Row>
-          </Grid>
-        </Card>
+          <Grid.Row>
+            <EventList events={user.createdEvents} title="Event title..." />
+          </Grid.Row>
+        </Grid>
       </Container>
     </main>
   );
